1718515961c85585b0b76623033671709ac33d6f
meha <meha@MEHA>
2023-04-18 16:40:52 +0800

    add page "EventCity"

--- CuratorEsstu/Controllers/TeacherController.cs
+++ CuratorEsstu/Controllers/TeacherController.cs
@@ -1,15 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
-
+using BusinessLayer;
+using Microsoft.AspNetCore.Identity;
+using DataLayer.Entity;
+using PresentationLayer;
 
 namespace CuratorEsstu.Controllers
 {
     [Authorize(Roles = "Куратор")]
     public class TeacherController : Controller
     {
-        public IActionResult Index()
+        DataManager _dataManager;
+        UserManager<User> _userManager;
+        SignInManager<User> _signInManager;
+        ServiceManager _serviceManager;
+        public TeacherController(DataManager dataManager, UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _dataManager = dataManager;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _serviceManager = new ServiceManager(dataManager);
+        }
+
+        public static class GlobalTeacher
+        {
+            public static User Teacher { get; set; }
+        }
+
+        public async Task<IActionResult> CreateUser()
+        {
+            GlobalTeacher.Teacher = await _userManager.GetUserAsync(User);
+            return RedirectToAction("Index", "Teacher");
+        }
+
+        public async Task<IActionResult> Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> EventCity()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Autorization");
         }
     }
 }
